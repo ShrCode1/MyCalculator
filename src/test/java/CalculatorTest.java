b5ee1c15a0024a1e27fa4ea5928394fa78d27d93
ShrCode1 <133050819+ShrCode1@users.noreply.github.com>
2024-09-24 04:01:42 +0530

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,45 +1,45 @@
 import org.example.Main;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert;
 
-public class CalculatorTest 
-{
-  private Main calculator;
-  @Before
-  public void setUp()
-  {
-    calculator = new Main();
-  }
-  @Test
-  public void test_add_positive()
-  {
-    int a = 1;
-    int b = 2;
-    int expectedResult = 3;
-    Assert.assertEquals(expectedResult, calculator.add(a,b));
-  }
-  @Test
-  public void test_add_negative()
-  {
-    int a = 1;
-    int b = 2;
-    int expectedResult = 0;
-    Assert.assertNotEquals(expectedResult, calculator.add(a,b));
-  }
-  @Test
-  public void test_sub_positive()
-  {
-    int a = 2;
-    int b = 2;
-    int expectedResult = 0;
-    Assert.assertEquals(expectedResult, calculator.sub(a,b));
-  }
-  @Test
-  public void test_sub_negative()
-  {
-  int a = 2;
-  int b = 2;
-  int expectedResult = 1;
-  Assert.assertNotEquals(expectedResult, calculator.sub(a,b));
-  }
+public class CalculatorTest {
+    private Main calculator;
+
+    @Before
+    public void setUp() {
+        calculator = new Main();
+    }
+
+    @Test
+    public void test_add_positive() {
+        int a = 1;
+        int b = 2;
+        int expectedResult = 3;
+        Assert.assertEquals(expectedResult, calculator.add(a, b));
+    }
+
+    @Test
+    public void test_add_negative() {
+        int a = 1;
+        int b = 2;
+        int expectedResult = 0;
+        Assert.assertNotEquals(expectedResult, calculator.add(a, b));
+    }
+
+    @Test
+    public void test_sub_positive() {
+        int a = 2;
+        int b = 2;
+        int expectedResult = 0;
+        Assert.assertEquals(expectedResult, calculator.sub(a, b));
+    }
+
+    @Test
+    public void test_sub_negative() {
+        int a = 2;
+        int b = 2;
+        int expectedResult = 1;
+        Assert.assertNotEquals(expectedResult, calculator.sub(a, b));
+    }
 }
