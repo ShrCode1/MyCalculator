6f92f095d8bd946b09b2cec01bf430303e5d7333
ShrCode1 <133050819+ShrCode1@users.noreply.github.com>
2024-09-24 03:15:50 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
+public class Main 
+{
+    private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(String[] args) 
+    {
         System.out.println("This my calculator.");
     }
 }
