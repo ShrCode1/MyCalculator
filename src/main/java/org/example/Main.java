932045ceb3565715f100764fd16b3c328da1404e
ShrCode1 <133050819+ShrCode1@users.noreply.github.com>
2024-09-24 04:02:22 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,20 @@
 package org.example;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.Scanner;
-public class Main 
-{
+
+public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args) 
-    {
-        System.out.println("This my calculator.");
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is my calculator.");
     }
 }
